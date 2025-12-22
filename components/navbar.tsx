9cc5c6589f41e7529acbe09aea8e332a65eaab3f
ChristosApatsidis <christos.apatsidis@icloud.com>
2025-12-22 02:11:22 +0200

    Add sizes attribute to logo image in Navbar component for better responsiveness

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -116,6 +116,7 @@ export function Navbar({ className }: { className?: string }) {
                   src="/logo.png"
                   alt="Logo"
                   fill
+                  sizes="32px"
                   className="object-contain"
                   priority
                 />
